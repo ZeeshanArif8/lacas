625288d55563d795a8099d3e15256bf2eafb9940
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2022-10-05 11:12:13 +0500

    cus_report

---
+++ cus_report/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name": "Challan",
+
+    "author": "Cognative",
+    "category": "CRM",
+
+    "license": "OPL-1",
+
+    "version": "15.0.1",
+
+    "depends": [
+        'account_accountant'
+    ],
+
+    "data": [
+        'views/main_view.xml',
+        'reports/sale_agrement.xml',
+        'reports/sale_agrement_template.xml',
+        'reports/admission_challan.xml',
+    ],
+    "images": [ ],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+    "price": "60",
+    "currency": "EUR",
+}
