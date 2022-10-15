54dcccd9d62439bee55b949ed8d53131db9b05d2
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2022-10-15 17:13:59 +0500

    Update main_view.py

--- challan_field/models/main_view.py
+++ challan_field/models/main_view.py
@@ -1,16 +1,19 @@
-from odoo import models, fields,api
+from odoo import models, fields, api
 from odoo.exceptions import UserError
+
 
 class account_fields(models.Model):
     _inherit = "account.move"
-    room  = fields.Char("Class")
-    
-     
-    
+    room = fields.Char("Class")
+
     photograph_charges = fields.Char("Photograph Charges")
     student_security = fields.Char("Student Security")
     admission_fee = fields.Char("Admission Fee")
-    father_name = fields.Char("Father Name")  
-    start_session = fields.Date("Start Session" )
+    father_name = fields.Char("Father Name")
+    start_session = fields.Date("Start Session")
     end_session = fields.Date("End Session")
     Registration_id = fields.Char("Registration ID")
+    discount_note = fields.Char("Add Discount note")
+
+    reject_reason = fields.Selection(
+        [('siblings', 'Siblings Discount'), ('Scholarship', 'Scholarship Discount'), ], 'Reason For Discount')
