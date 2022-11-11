cc54dd9298e075913dbe8c2ecb00cf7d7ccb637f
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2022-11-11 19:26:44 +0500

    custom_withdrawal

--- custom_withdrawals/models/lacas_model.py
+++ custom_withdrawals/models/lacas_model.py
@@ -14,26 +14,62 @@ class academics_tab(models.Model):
     financial_responsibilty_cred_custom = fields.Selection(
         [('Yes', 'Yes'), ('No', 'No')], string="Financial Responsibilty")
 
+    notice_fee_withdrawal = fields.Monetary(
+        compute='_compute_notice_fee', string="Notice Fee")
+    amount_total_withdrawal = fields.Monetary(
+        compute='_compute_total_amount', string="total withdrawal")
     refund_receive = fields.Char(
         compute='_compute_refund_receive', string="Receivable/Refundable")
+
+    def _compute_notice_fee(self):
+        if self.move_type == "out_refund":
+            if self.x_studio_charges:
+                self.notice_fee_withdrawal = self.x_studio_charges.invoice_line_ids.price_subtotal
+
+    def _compute_total_amount(self):
+        if self.move_type == "out_refund":
+            self.amount_total_withdrawal = abs(
+                self.notice_fee_withdrawal-self.invoice_line_ids.price_subtotal)
 
     def _compute_refund_receive(self):
 
         if self.x_studio_charges:
 
-            if self.x_studio_charges.invoice_line_ids:
+            if self.move_type == "out_refund":
+                for i in self.x_studio_charges.invoice_line_ids:
+                    receive = i.price_subtotal
+                for j in self.invoice_line_ids:
+                    refund = j.price_subtotal
+                if receive > refund:
+                    self.refund_receive = "Receivable"
+                else:
+                    self.refund_receive = "Refundable"
 
+            if self.move_type == "out_invoice":
                 for i in self.x_studio_charges.invoice_line_ids:
                     refund = i.price_subtotal
-            if self.invoice_line_ids:
                 for j in self.invoice_line_ids:
                     receive = j.price_subtotal
+                if receive > refund:
+                    self.refund_receive = "Receivable"
+                else:
+                    self.refund_receive = "Refundable"
 
-            if receive > refund:
-                self.refund_receive = 'Receivable'
-            else:
-                self.refund_receive = 'Refundable'
-        else:
-            self.refund_receive = 'Refundable'
+        # if self.x_studio_charges:
+
+        #     if self.x_studio_charges.invoice_line_ids:
+
+        #         for i in self.x_studio_charges.invoice_line_ids:
+        #             refund = i.price_subtotal
+        #     if self.invoice_line_ids:
+        #         for j in self.invoice_line_ids:
+        #             receive = j.price_subtotal
+
+        #     if receive > refund:
+        #         self.refund_receive = 'Receivable'
+        #     else:
+        #         self.refund_receive = 'Refundable'
+        # else:
+        #     self.refund_receive = 'Refundable'
 
 #   record.write({'x_receivable_refundable': 'receivable'})
