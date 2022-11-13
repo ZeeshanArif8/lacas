3f7af62ae895701edcca1c78dc880c3929340189
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2022-11-13 20:22:29 +0500

    withdrawal_update

--- custom_withdrawals/models/lacas_model.py
+++ custom_withdrawals/models/lacas_model.py
@@ -14,17 +14,19 @@ class academics_tab(models.Model):
     financial_responsibilty_cred_custom = fields.Selection(
         [('Yes', 'Yes'), ('No', 'No')], string="Financial Responsibilty")
 
-    # notice_fee_withdrawal = fields.Monetary(
-    #     compute='_compute_notice_fee', string="Notice Fee")
+    notice_fee_withdrawal = fields.Monetary(
+        compute='_compute_notice_fee', string="Notice Fee")
     # amount_total_withdrawal = fields.Monetary(
     #     compute='_compute_total_amount', string="total withdrawal")
     refund_receive = fields.Char(
         compute='_compute_refund_receive', string="Receivable/Refundable")
 
-    # def _compute_notice_fee(self):
-    #     if self.move_type == "out_refund":
-    #         if self.x_studio_charges:
-    #             self.notice_fee_withdrawal = self.x_studio_charges.invoice_line_ids.price_subtotal
+    def _compute_notice_fee(self):
+        # if self.move_type == "out_refund":
+        if self.x_studio_charges:
+            self.notice_fee_withdrawal = self.x_studio_charges.invoice_line_ids.price_subtotal
+        else:
+            self.notice_fee_withdrawal = 0
 
     # def _compute_total_amount(self):
     #     if self.move_type == "out_refund":
