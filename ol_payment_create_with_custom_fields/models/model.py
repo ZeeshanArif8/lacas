2525d3d5ccf872f9e4c80959ca0d49bbe4850412
Usama Shahid <itisusamashahid@gmail.com>
2022-11-15 10:22:56 +0000

    added ol_payment_create_with_custom_fields
    added ol_payment_create_with_custom_fields

---
+++ ol_payment_create_with_custom_fields/models/model.py
@@ -0,0 +1,21 @@
+
+from odoo import models, api, fields, _
+from odoo.exceptions import UserError
+
+
+
+
+class extwiz(models.TransientModel):
+    _inherit = "account.payment.register"
+    ol_check_in_favor_of = fields.Char('Check in Favor Of')
+    
+    @api.model
+    def _create_payments(self):
+        payments=super(extwiz,self)._create_payments()
+
+        for payment in payments:
+            payment.ol_check_in_favor_of = self.ol_check_in_favor_of
+        return payments
+class extpayment(models.Model):
+    _inherit = "account.payment"
+    ol_check_in_favor_of = fields.Char('Check in Favor Of')
