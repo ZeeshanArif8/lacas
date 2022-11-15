cd1b29d3e6b7bd81f25e44eec41f2c077914f80f
Usama Shahid <itisusamashahid@gmail.com>
2022-11-15 08:48:12 +0000

    added ol_payment_create_with_custom_fields.
    added ol_payment_create_with_custom_fields.

---
+++ ol_payment_create_with_custom_fields/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Payment Create With Custom Fields',
+
+    "author": "Usama Shahid",
+    'version': '0.1',
+    'category': 'Payment Wizard Customization',
+    'sequence': 95,
+    'summary': 'Customize register payment wizard to add custom fields. ',
+    'description': "Customize register payment wizard to add custom fields. ",
+    'website': '',
+    'images': [
+    ],
+    'depends': ["mrp",
+        
+    ],
+    'data': [
+        
+        "views/view.xml",
+        
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [
+    ],
+    'license': 'LGPL-3',
+}
---
+++ ol_payment_create_with_custom_fields/models/model.py
@@ -0,0 +1,20 @@
+
+from odoo import models, api, fields, _
+from odoo.exceptions import UserError
+
+
+
+
+class extwiz(models.TransientModel):
+    _inherit = "account.payment.register"
+    ol_check_in_favor_of = fields.Char('check in favor of')
+    @api.model
+    def _create_payments(self):
+        payments=super(ext,self)._create_payments()
+
+        for payment in payments:
+            payment.x_check_in_favor_of = self.ol_check_in_favor_of
+        return payments
+class extpayment(models.Model):
+    _inherit = "account.payment"
+    ol_check_in_favor_of = fields.Char('check in favor of')
